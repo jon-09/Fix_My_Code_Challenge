0c36bf3af5e356bd453af47dd4cc233cfd62fa03
Jonathan <achola.jon@yahoo.com>
2022-09-16 13:56:49 +0300

    user

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -54,7 +54,7 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
 
 
 if __name__ == '__main__':
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     u_pwd = "myPassword"
     user_1.password = u_pwd
     if user_1.password == u_pwd:
-        print("User.password should be hashed"), pwd
+        print("User.password should be hashed")
 
     if user_2.password is not None:
         print("User.password should be None by default")
