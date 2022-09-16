e0fa1a4a0f946adf3e7863ed9e1df7ee38425d9d
Jonathan <achola.jon@yahoo.com>
2022-09-16 13:51:07 +0300

    user

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     u_pwd = "myPassword"
     user_1.password = u_pwd
     if user_1.password == u_pwd:
-        print("User.password should be hashed")
+        print("User.password should be hashed"), pwd
 
     if user_2.password is not None:
         print("User.password should be None by default")
@@ -85,10 +85,12 @@ if __name__ == '__main__':
         print("User.password should be None if setter to an integer")
 
     if not user_1.is_valid_password(u_pwd):
-        print("is_valid_password should return True if it's the right password")
+        print("is_valid_password should return True if it's the right \
+password")
 
     if user_1.is_valid_password("Fakepwd"):
-        print("is_valid_password should return False if it's not the right password")
+        print("is_valid_password should return False if it's not the right \
+password")
 
     if user_1.is_valid_password(None):
         print("is_valid_password should return False if compare with None")
@@ -97,4 +99,5 @@ if __name__ == '__main__':
         print("is_valid_password should return False if compare with integer")
 
     if user_2.is_valid_password("No pwd"):
-        print("is_valid_password should return False if no assword set before")
+        print("is_valid_password should return False if no password set \
+before")
